361d3f5774096d29d3d8ab2f14232450b28dcc4d
William Todt <william.todt@mailbox.org>
2021-03-23 21:33:07 +0100

    [CORE] Print receipt items

--- src/receipt_printer.py
+++ src/receipt_printer.py
@@ -1,20 +1,28 @@
 import util as u
 
+
 # Send info message
 def info(skk): print("\033[95m{}\033[00m".format(skk))
+
 
 # Send error message
 def error(skk): print("\033[91m{}\033[00m".format(skk))
 
+
 # Print receipt list
 def print_receipt_list(receipt):
     if not receipt.items:
-        return None
+        return
+
+    for item in receipt.items:
+        print("\t*", item.article, " ", item.sum)
+
 
 # Print full receipt
 def print_receipt(receipt):
     print("Company:    ", u.save_ret(receipt.market))
     print("Date:       ", u.save_ret(receipt.date))
     print("Amount:     ", u.save_ret(receipt.sum))
-    print("Items:     ", print_receipt_list(receipt))
+    print("Items:     ")
 
+    print_receipt_list(receipt)
