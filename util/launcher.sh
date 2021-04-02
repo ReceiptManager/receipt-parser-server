24f26b5e9d33948831b3691870638db09b7cd72c
William Todt <william.todt@mailbox.org>
2021-04-02 22:15:12 +0200

    [CORE] enhance getIP method and correct Docker image name

--- util/launcher.sh
+++ util/launcher.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-IP=$(echo -n $(hostname --ip-address | tr " " "\n" | grep -v 172.17.0.1))
+IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+
 echo "Current IP is: $IP"
-docker run --rm -t -i -p $IP:8721:8721 -v "$(pwd)/data:/app/data" -v "$(pwd):/config" -e RECEIPT_PARSER_CONFIG_DIR="/config" monolidth/receipt-parser-server
+docker run --rm -t -i -p $IP:8721:8721 -v "$(pwd)/data:/app/data" -v "$(pwd):/config" -e RECEIPT_PARSER_CONFIG_DIR="/config" monolidth/receipt-parser
