32a794ed5f9ff60996fcc54c4b7b16e813b6042d
William <william.todt@mailbox.org>
2021-04-05 20:14:58 +0200

    Create data folder if it does not exist.
    Fixes #31

--- util/launcher.sh
+++ util/launcher.sh
@@ -3,4 +3,9 @@
 IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 
 echo "Current IP is: $IP"
+
+mkdir -p data/tmp
+mkdir -p data/img
+mkdir -p data/out
+
 docker run --rm -t -i -p $IP:8721:8721 -v "$(pwd)/data:/app/data" -v "$(pwd):/config" -e RECEIPT_PARSER_CONFIG_DIR="/config" monolidth/receipt-parser
